25312c10e3bae5ae4a6b160543ced9e00d580d2b
PavanKumar Vaduguru <vakumar@qualys.com>
2023-01-10 16:08:14 +0530

    Add a message

--- src/main/java/com/example/RestfulAPI/RestfulApiApplication.java
+++ src/main/java/com/example/RestfulAPI/RestfulApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestfulApiApplication {
 
 	public static void main(String[] args) {
-
+		System.out.println("Welcome to CRUD API");
 		SpringApplication.run(RestfulApiApplication.class, args);
 	}
 
